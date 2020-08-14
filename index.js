0285191c4629a8997f9527363a8e52a8f9288ba7
yulia <juliana.ny2008@gmail.com>
2020-08-14 12:38:34 -0400

    Done.

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ class Breakfast {
     constructor(food, drink) {
       this.food = food
       this.drink = drink
-    }
-  }
+    }}
   
   class Lunch {
     constructor(salad, soup, drink) {
       this.salad = salad
       this.soup = soup
       this.drink = drink
-    }
-  }
+    }}
   
   class Dinner {
     constructor(salad, soup, entree, dessert) {
@@ -19,5 +17,4 @@ class Breakfast {
       this.soup = soup 
       this.entree = entree 
       this._dessert = dessert
-    }
-  }
+    }}
